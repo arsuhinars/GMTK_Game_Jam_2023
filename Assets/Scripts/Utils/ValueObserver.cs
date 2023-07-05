3fecc0aac759a9547c8efaebd88b1be174cb1e29
arsuhinars <arsuhinars@gmail.com>
2023-07-05 22:06:06 +0400

    Added ValueObserver utility class

---
+++ Assets/Scripts/Utils/ValueObserver.cs
@@ -0,0 +1,33 @@
+﻿
+namespace GMTK_2023.Utils
+{
+    public delegate void ChangeEvent<T>(T oldValue, T newValue);
+
+    public class ValueObserver<T>
+    {
+        public event ChangeEvent<T> OnValueChanged;
+
+        public T Value
+        {
+            get => m_value;
+            set
+            {
+                if (!m_value.Equals(value))
+                {
+                    T oldValue = m_value;
+                    m_value = value;
+                    OnValueChanged?.Invoke(oldValue, value);
+                }
+            }
+        }
+
+        private T m_value = default;
+
+        public ValueObserver() { }
+
+        public ValueObserver(T value)
+        {
+            m_value = value;
+        }
+    }
+}
