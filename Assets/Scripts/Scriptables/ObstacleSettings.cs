61e167d9583b71ef770cb0a666bd72a9941e8925
arsuhinars <arsuhinars@gmail.com>
2023-07-08 02:14:34 +0400

    Added scriptables classes

---
+++ Assets/Scripts/Scriptables/ObstacleSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GMTK_2023.Scriptables
+{
+    [CreateAssetMenu(fileName = "ObstacleSettings", menuName = "Game/Behaviours/Obstacle Settings")]
+    public class ObstacleSettings : ScriptableObject
+    {
+        public string playerTag;
+    }
+}
