1c8fe7170c1f8e8dd0eeb6439c19f8a695287884
arsuhinars <arsuhinars@gmail.com>
2023-07-08 02:13:58 +0400

    Added TagUtils

---
+++ Assets/Scripts/Utils/TagUtils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GMTK_2023.Utils
+{
+    public static class TagUtils
+    {
+        public static bool CompareTagsFromArray(Component component, string[] tags)
+        {
+            for (int i = 0; i < tags.Length; i++)
+            {
+                if (component.CompareTag(tags[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
