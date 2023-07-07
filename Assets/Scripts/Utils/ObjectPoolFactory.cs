f526a390a06703da2eea6a3257d61e1835ac35eb
arsuhinars <arsuhinars@gmail.com>
2023-07-08 02:14:13 +0400

    Improved pool factory

--- Assets/Scripts/Utils/ObjectPoolFactory.cs
+++ Assets/Scripts/Utils/ObjectPoolFactory.cs
@@ -1,3 +1,4 @@
+using GMTK_2023.Behaviours;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -5,30 +6,39 @@ namespace GMTK_2023.Utils
 {
     public class ObjectPoolFactory
     {
-        public static IObjectPool<T> CreatePrefabsPool<T>(T prefab, Transform parent=null) where T : Component
+        public static ObjectPool<PoolItem> CreatePrefabsPool(PoolItem prefab, Transform parent=null)
         {
-            return new ObjectPool<T>(
-                // On create
-                () =>
-                {
-                    return Object.Instantiate(prefab, parent);
-                },
-                // On get
-                (T obj) =>
-                {
-                    obj.gameObject.SetActive(true);
-                },
-                // On release
-                (T obj) =>
-                {
-                    obj.gameObject.SetActive(false);
-                },
-                // On destroy
-                (T obj) =>
-                {
-                    Object.Destroy(obj);
-                }
-            );
+            ObjectPool<PoolItem> pool = null;
+
+            PoolItem OnCreate()
+            {
+                var obj = Object.Instantiate(prefab, parent);
+                obj.CurrentPool = pool;
+                return obj;
+            }
+
+            void OnGet(PoolItem obj)
+            {
+                obj.gameObject.SetActive(true);
+                obj.IsActiveInPool = true;
+                obj.OnGet();
+            }
+
+            void OnRelease(PoolItem obj)
+            {
+                obj.gameObject.SetActive(false);
+                obj.IsActiveInPool = false;
+                obj.OnRelease();
+            }
+
+            void OnDestroy(PoolItem obj)
+            {
+                Object.Destroy(obj.gameObject);
+            }
+
+            pool = new ObjectPool<PoolItem>(OnCreate, OnGet, OnRelease, OnDestroy);
+
+            return pool;
         }
     }
 }
