30814146082f188b8baf062460caaea489a1346c
arsuhinars <arsuhinars@gmail.com>
2023-07-08 02:15:14 +0400

    Added behaviour classes

---
+++ Assets/Scripts/Behaviours/ObstacleEntity.cs
@@ -0,0 +1,18 @@
+using GMTK_2023.Scriptables;
+using UnityEngine;
+
+namespace GMTK_2023.Behaviours
+{
+    public class ObstacleEntity : LevelItem
+    {
+        [SerializeField] private ObstacleSettings m_settings;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(m_settings.playerTag))
+            {
+                other.GetComponent<ISpawnable>().Kill();
+            }
+        }
+    }
+}
