e812df0ab73e479cf8fa0f02d17124b4e7562396
arsuhinars <arsuhinars@gmail.com>
2023-07-09 19:43:47 +0400

    Added SoundManager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,19 +1,52 @@
+using GMTK_2023.Utils;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace GMTK_2023.Managers
 {
     public enum SoundEffect
     {
-        Throw, Explosion, Fishrod, WaterSplash
+        UIClick
     }
 
     public class SoundManager : MonoBehaviour
     {
         public static SoundManager Instance { get; private set; } = null;
 
+        public ValueObserver<bool> IsSoundOn => m_isSoundOn;
+
+        [SerializeField] private Transform m_audioSourceRoot;
+        [SerializeField] private AudioSource m_audioSourcePrefab;
+        [SerializeField] private AudioClip m_uiClickEffect;
+        [Space]
+        [SerializeField] private AudioSource[] m_sceneSources;
+
+        private ValueObserver<bool> m_isSoundOn = new();
+        private ObjectPool<AudioSource> m_pool;
+        private LinkedList<AudioSource> m_activeSources = new();
+
         public void PlaySound(SoundEffect effect)
         {
+            switch (effect)
+            {
+                case SoundEffect.UIClick:
+                    PlaySound(m_uiClickEffect);
+                    break;
+            }
+        }
 
+        public void PlaySound(AudioClip clip)
+        {
+            if (!m_isSoundOn.Value)
+            {
+                return;
+            }
+
+            var source = m_pool.Get();
+            source.clip = clip;
+            source.Play();
+            m_activeSources.AddLast(source);
         }
 
         private void Awake()
@@ -31,20 +64,55 @@ namespace GMTK_2023.Managers
 
         private void Start()
         {
-            GameManager.Instance.OnStart += OnGameStart;
+            m_isSoundOn.Value = PlayerPrefs.GetInt("IsSoundOn", 1) != 0;
+            m_isSoundOn.OnValueChanged += (oldVal, newVal) =>
+            {
+                PlayerPrefs.SetInt("IsSoundOn", newVal ? 1 : 0);
+                UpdateSceneSources();
+            };
+
+            m_pool = new ObjectPool<AudioSource>(
+                () => Instantiate(m_audioSourcePrefab, m_audioSourceRoot),
+                (source) => { },
+                (source) => source.Stop(),
+                (source) => Destroy(source.gameObject)
+            );
+
+            UpdateSceneSources();
         }
 
-        private void OnDestroy()
+        private void Update()
         {
-            if (GameManager.Instance != null)
+            var it = m_activeSources.First;
+            while (it != null)
             {
-                GameManager.Instance.OnStart -= OnGameStart;
+                var t = it.Value;
+                if (!t.isPlaying)
+                {
+                    var oldIt = it;
+                    it = it.Next;
+                    m_pool.Release(t);
+                    m_activeSources.Remove(oldIt);
+                    continue;
+                }
+
+                it = it.Next;
             }
         }
 
-        private void OnGameStart()
+        private void UpdateSceneSources()
         {
-
+            foreach (var src in m_sceneSources)
+            {
+                if (m_isSoundOn.Value)
+                {
+                    src.Play();
+                }
+                else
+                {
+                    src.Pause();
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/UI/Elements/SoundButton.cs
@@ -0,0 +1,60 @@
+﻿using GMTK_2023.Managers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GNT.UI.Elements
+{
+    [RequireComponent(typeof(Image))]
+    public class SoundButton : Selectable
+    {
+        [SerializeField] private Sprite m_onImage;
+        [SerializeField] private Sprite m_offImage;
+
+        private Image m_image;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            m_image = GetComponent<Image>();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            SoundManager.Instance.IsSoundOn.OnValueChanged += OnSoundStateChange;
+            UpdateImage();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            if (SoundManager.Instance != null)
+            {
+                SoundManager.Instance.IsSoundOn.OnValueChanged -= OnSoundStateChange;
+            }
+        }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+
+            var soundState = SoundManager.Instance.IsSoundOn;
+            soundState.Value = !soundState.Value;
+        }
+
+        private void OnSoundStateChange(bool oldVal, bool newVal)
+        {
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            bool soundState = SoundManager.Instance.IsSoundOn.Value;
+            m_image.sprite = soundState ? m_onImage : m_offImage;
+        }
+    }
+}
